9c84ffba445ad71cc3760c7e393bf35fe09262a8
stuti <stutipandey1090@gmail.com>
2021-07-11 13:17:19 +0530

    Initial

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const db =
+  "mongodb+srv://stuti:Stutipanda@1090@cluster0.9zbip.mongodb.net/mernstack?retryWrites=true&w=majority";
+mongoose
+  .connect(db,{
+     useNewUrlParser: true ,
+     useCreateIndex: true,
+     useUnifiedTopology:true,
+     useFindAndModify:false 
+  })
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.get("/", (req, res) => {
+  res.send(`Hello world`);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`Hello about`);
+});
+
+app.get("/contact", (req, res) => {
+  res.send(`Hello contact`);
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`Hello signin`);
+});
+
+app.get("/signup", (req, res) => {
+  res.send(`Hello signup`);
+});
+
+app.listen(3000, () => {
+  console.log("server is running at port");
+});
